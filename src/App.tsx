ec48b6614c781a1dd2c70396ea461e63a14131df
Ranjith K <thisisranjithk@gmail.com>
2024-11-14 10:51:30 +0530

    fetch data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import ReminderList from "./components/ReminderList";
+import { Todo } from "./types/reminders";
+import { getTodos } from "./components/services/reminders";
+import ReminderFrom from "./components/ReminderFrom";
+
 function App() {
-  return <button className="btn btn-primary">Click</button>;
+  const [reminders, setReminders] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await getTodos();
+      setReminders(response.data);
+    };
+
+    fetchTodos();
+  }, []);
+  return (
+    <>
+      <ReminderFrom />
+      <ReminderList items={reminders} />
+    </>
+  );
 }
 
 export default App;
