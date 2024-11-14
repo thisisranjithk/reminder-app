e0c216072cf096dfb1ccee25d523653408f0b5b4
Ranjith K <thisisranjithk@gmail.com>
2024-11-14 10:49:48 +0530

    axios instance for reminders api

---
+++ src/components/services/reminders.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { Todo } from "../../types/reminders";
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com", // Base URL for the mock API
+  timeout: 10000, // Request timeout in milliseconds
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getTodos = (): Promise<AxiosResponse<Todo[]>> =>
+  axiosInstance.get("/todos");
+
+export const getTodoById = (id: number): Promise<AxiosResponse<Todo[]>> =>
+  axiosInstance.get(`/todos/${id}`);
+
+export const createTodo = (data: Todo): Promise<AxiosResponse<Todo[]>> =>
+  axiosInstance.post("/todos", data);
+
+export const updateTodo = (
+  id: number,
+  data: Todo
+): Promise<AxiosResponse<Todo[]>> => axiosInstance.put(`/todos/${id}`, data);
+
+export const deleteTodo = (id: number): Promise<AxiosResponse<void>> =>
+  axiosInstance.delete(`/todos/${id}`);
